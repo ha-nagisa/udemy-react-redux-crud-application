81177a6093b186a558a5993e98d3e36a64b959e4
ha-nagisa <nagisa18222@icloud.com>
2021-01-21 23:36:27 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {creatStore} from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
