0b33a5fb59ff4a96c93c87dd92a0f20a92637a2b
ha-nagisa <nagisa18222@icloud.com>
2021-01-22 21:29:58 +0900

    implement component width connect function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {creatStore} from 'redux';
+import {createStore} from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import reducer from './reducers';
